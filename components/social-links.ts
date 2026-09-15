393b5be7e8b0fe6f8d54c1498e4a7ba9e051834f
Seth Saddih <saddihseth@gmail.com>
2026-09-15 13:46:49 +0100

    Fix shared social links data boundary for Contact page

---
+++ components/social-links.ts
@@ -0,0 +1,8 @@
+import { Github, Instagram, Linkedin, Twitter } from "lucide-react";
+
+export const socialLinks = [
+  { label: "GitHub", href: "https://github.com/sethsaddih", icon: Github },
+  { label: "LinkedIn", href: "https://www.linkedin.com/in/seth-saddih-862444371", icon: Linkedin },
+  { label: "Instagram", href: "https://www.instagram.com/seth_saddih", icon: Instagram },
+  { label: "X", href: "https://x.com/sethsaddih", icon: Twitter },
+];
